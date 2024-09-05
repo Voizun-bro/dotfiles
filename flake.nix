de57343a1bf00b9ae8548510dc7e19ed1337697e
Voizun-bro <161002225+Voizun-bro@users.noreply.github.com>
2024-09-06 01:26:23 +0900

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "VOIZUN's dotfiles";
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+      
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    catppuccin.url = "github:catppuccin/nix"
+      
+  }:
+
+  outputs = inputs: {};
+  
+}
