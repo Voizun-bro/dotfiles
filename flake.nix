60a9ac9f602791f16df07ab978dbe25628c36a56
Voizun-bro <161002225+Voizun-bro@users.noreply.github.com>
2024-09-07 09:59:45 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,9 +21,36 @@
     };
 
     catppuccin.url = "github:catppuccin/nix"
-      
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
   };
 
-  outputs = inputs: {};
-  
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    # The host with the hostname `my-nixos` will use this configuration
+    nixosConfigurations = {
+      gpd-p2-max = let
+        username = "voizun";
+        specialArgs = {inherit username;};
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          system = "x86_64-linux";
+
+          modules = [
+            ./hosts/gpd-p2-max
+            ./users/${username}/nixos.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            }
+          ];
+        };
+      }
+
 }
